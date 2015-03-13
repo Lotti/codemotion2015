25b170bd811d395c8552f9fa54a68ed9e968ba8b
Lotti <lotti86@gmail.com>
2015-03-13 20:40:42 +0100

    added socket.io and server files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var server = require('../server/server.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
---
+++ server/server.js
@@ -0,0 +1,17 @@
+function server(io) {
+    this.io = io;
+    
+    io.on('connection', function(socket){
+      console.log('a user connected');
+    });    
+      socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });    
+}
+
+server.prototype.checkRequest = function (id) {
+	return { type: "a", id: id };
+};
+
+
+module.exports = server;
