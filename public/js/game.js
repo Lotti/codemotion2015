78d16c47b1acd1f16b09d69cafa33cfe8515034c
Lotti <lotti86@gmail.com>
2015-03-16 23:49:08 +0100

    client - server development (refactoring...)

--- public/js/game.js
+++ public/js/game.js
@@ -27,19 +27,19 @@ function Pong() {
     var game = new Phaser.Game(gameWidth, gameHeight, debug ? Phaser.CANVAS : Phaser.AUTO, gameDiv, null, false, false);
 
     var host = false;
-    var players = [];
+    var paddles = [];
     var sprites;
     var ball;
 
-    var player = (debug == true ? 0 : -1);
-    var master = false;//(player == 0);
-
+    var currentPlayer = -1;
+    var master = false;
+    var cursors;
 
     var colors = ["ff0000", "00ff00", "0000ff", "ffff00"];
 
     function demoMovements () {
-        for (var i in players) {
-            var p = players[i];
+        for (var i in paddles) {
+            var p = paddles[i];
             var pH2 = p.body.height;
             var pW2 = p.body.width;
             switch (parseInt(i)) {
@@ -98,38 +98,38 @@ function Pong() {
             ball.body.minBounceVelocity = 0;
             ball.player = -1;
 
-            players = [];
-            players.push(sprites.create(game.width / 2, 5, 'pixel'));
-            players.push(sprites.create(5, game.height / 2, 'pixel'));
-            players.push(sprites.create(game.width / 2, game.height - 5, 'pixel'));
-            players.push(sprites.create(game.width - 5, game.height / 2, 'pixel'));
+            paddles = [];
+            paddles.push(sprites.create(game.width / 2, 5, 'pixel'));
+            paddles.push(sprites.create(5, game.height / 2, 'pixel'));
+            paddles.push(sprites.create(game.width / 2, game.height - 5, 'pixel'));
+            paddles.push(sprites.create(game.width - 5, game.height / 2, 'pixel'));
 
-            players[0].tint = 0xff0000;
-            players[1].tint = 0x00ff00;
-            players[2].tint = 0x0000ff;
-            players[3].tint = 0xffff00;
+            paddles[0].tint = 0xff0000;
+            paddles[1].tint = 0x00ff00;
+            paddles[2].tint = 0x0000ff;
+            paddles[3].tint = 0xffff00;
 
-            for (var i in players) {
-                players[i].player = i;
-                players[i].name = 'player' + (i + 1);
+            for (var i in paddles) {
+                paddles[i].player = i;
+                paddles[i].name = 'player' + (i + 1);
                 if (i % 2 == 0) {
-                    players[i].scale.setTo(50, 10);
+                    paddles[i].scale.setTo(50, 10);
                 }
                 else {
-                    players[i].scale.setTo(10, 50);
+                    paddles[i].scale.setTo(10, 50);
                 }
-                players[i].anchor.setTo(0.5, 0.5);
-                game.physics.enable([players[i]], Phaser.Physics.ARCADE);
-                players[i].body.bounce.x = 1;
-                players[i].body.bounce.y = 1;
-                players[i].body.minBounceVelocity = 0;
-                players[i].body.immovable = true;
+                paddles[i].anchor.setTo(0.5, 0.5);
+                game.physics.enable([paddles[i]], Phaser.Physics.ARCADE);
+                paddles[i].body.bounce.x = 1;
+                paddles[i].body.bounce.y = 1;
+                paddles[i].body.minBounceVelocity = 0;
+                paddles[i].body.immovable = true;
             }
 
             sprites.setAll('body.collideWorldBounds', true);
         },
         update: function () {
-            game.physics.arcade.collide(ball, players, function (ball, player) {
+            game.physics.arcade.collide(ball, paddles, function (ball, player) {
                 ball.tint = player.tint;
             });
 
@@ -138,10 +138,12 @@ function Pong() {
     };
 
     var SynchState = {
+        p: false,
         players: 0,
         countdown: false,
         init: function (data) {
             var self = this;
+            self.p = data.playersCount -1;
             self.players = parseInt(data.playersCount);
             if (data.hosting) {
                 host = true;
@@ -161,17 +163,25 @@ function Pong() {
                 ack(socket.id);
             });
         },
+        preload: function () {
+            cursors = game.input.keyboard.createCursorKeys();
+        },
         create: function () {
             var style = {font: "30px Arial", fill: "#ffffff", align: "center"};
             this.text = game.add.text(game.world.centerX, game.world.centerY, "Awaiting other players (" + this.players + "/4)", style);
             this.text.anchor.setTo(0.5, 0.5);
         },
         update: function () {
-            game.physics.arcade.collide(ball, players, function (ball, player) {
+            game.physics.arcade.collide(ball, paddles, function (ball, player) {
                 ball.tint = player.tint;
             });
 
-            demoMovements();
+            if (!this.countdown || this.countdown < 0) {
+                demoMovements();
+            }
+            else {
+                initGame(this.countdown);
+            }
 
             if (this.countdown) {
                 this.text.text = "Starting in "+this.countdown+"...";
@@ -182,6 +192,28 @@ function Pong() {
             else {
                 this.text.text = "Awaiting other players (" + this.players + "/4)";
             }
+        },
+        initGame: function(phase) {
+            switch(phase) {
+                case 1:
+                    ball.position.setTo(game.width / 2, game,height / 2);
+                    ball.tint = 0xffffff;
+                    ball.player = -1;
+                    ball.body.velocity.x = 0;
+                    ball.body.velocity.y = 0;
+                break;
+                case 2:
+                    this.text.text = "GO!";
+                    paddles[0].position.setTo(game.width / 2, 5);
+                    paddles[1].position.setTo(5, game.height / 2);
+                    paddles[2].position.setTo(game.width / 2, game.height - 5);
+                    paddles[3].position.setTo(game.width - 5, game.height / 2);
+                break;
+                case 3:
+                    this.text.destroy();
+                    game.state.start("game", false, false, { player: this.p });
+                break;
+            }
         }
     };
 
@@ -190,65 +222,18 @@ function Pong() {
     //TODO: trasferimento dati
     //TODO: endgame?
     var GameState = {
-        preload: function () {
-            this.cursors = game.input.keyboard.createCursorKeys();
+        init: function (data) {
+            currentPlayer = data.player;
+            master = data.player == 0;
+
+            socket.on('update', function(data) {
+                self.countdown = parseInt(data.times);
+            });
+            socket.on('input', function(data) {
+                self.countdown = parseInt(data.times);
+            });
         },
         create: function () {
-            if (master) {
-                game.physics.startSystem(Phaser.Physics.ARCADE);
-            }
-
-            sprites = game.add.group();
-
-            ball = sprites.create(game.width / 2, game.height / 2, 'pixel');
-            ball.name = 'ball';
-            ball.scale.setTo(10, 10);
-            ball.anchor.setTo(0.5, 0.5);
-
-            if (master) {
-                game.physics.enable([this.ball], Phaser.Physics.ARCADE);
-                ball.body.velocity.x = game.rnd.integerInRange(-200, 200);
-                ball.body.velocity.y = game.rnd.integerInRange(-200, 200);
-                ball.body.bounce.x = 1;
-                ball.body.bounce.y = 1;
-                ball.body.minBounceVelocity = 0;
-                ball.player = -1;
-            }
-
-            players = [];
-            players.push(sprites.create(game.width / 2, 5, 'pixel'));
-            players.push(sprites.create(5, game.height / 2, 'pixel'));
-            players.push(sprites.create(game.width / 2, game.height - 5, 'pixel'));
-            players.push(sprites.create(game.width - 5, game.height / 2, 'pixel'));
-
-            players[0].tint = 0xff0000;
-            players[1].tint = 0x00ff00;
-            players[2].tint = 0x0000ff;
-            players[3].tint = 0xffff00;
-
-            for (var i in this.players) {
-                players[i].player = i;
-                players[i].name = 'player' + (i + 1);
-                if (i % 2 == 0) {
-                    players[i].scale.setTo(50, 10);
-                }
-                else {
-                    players[i].scale.setTo(10, 50);
-                }
-                players[i].anchor.setTo(0.5, 0.5);
-                if (master) {
-                    game.physics.enable([players[i]], Phaser.Physics.ARCADE);
-                    players[i].body.bounce.x = 1;
-                    players[i].body.bounce.y = 1;
-                    players[i].body.minBounceVelocity = 0;
-                    players[i].body.immovable = true;
-                }
-            }
-
-            if (master) {
-                sprites.setAll('body.collideWorldBounds', true);
-            }
-
             var scoresPos = [
                 {w: game.world.centerX, h: game.world.centerY - 100},
                 {w: game.world.centerX - 100, h: game.world.centerY},
@@ -256,15 +241,15 @@ function Pong() {
                 {w: game.world.centerX + 100, h: game.world.centerY}
             ];
 
-            for (var i in players) {
+            for (var i in paddles) {
                 var style = {font: "50px Arial", fill: "#" + colors[i], align: "center"};
-                players[i].scoreLabel = game.add.text(scoresPos[i].w, scoresPos[i].h, "0", style);
-                players[i].scoreLabel.anchor.setTo(0.5, 0.5);
+                paddles[i].scoreLabel = game.add.text(scoresPos[i].w, scoresPos[i].h, "0", style);
+                paddles[i].scoreLabel.anchor.setTo(0.5, 0.5);
             }
         },
         update: function () {
             if (master) {
-                game.physics.arcade.collide(ball, players, function (ball, player) {
+                game.physics.arcade.collide(ball, paddles, function (ball, player) {
                     ball.tint = player.tint;
                     ball.player = player.player;
                 });
@@ -278,95 +263,98 @@ function Pong() {
             if (ball.body.y < 1) {
                 scored = true;
                 if (ball.player == -1 || ball.player == 0) {
-                    players[0].scoreLabel.text--;
+                    paddles[0].scoreLabel.text--;
                     scored = true;
                 }
                 else {
-                    players[ball.player].scoreLabel.text++;
+                    paddles[ball.player].scoreLabel.text++;
                 }
             }
             else if (ball.body.y > game.world.height - ball.body.height - 1) {
                 scored = true;
                 if (ball.player == -1 || ball.player == 2) {
-                    players[2].scoreLabel.text--;
+                    paddles[2].scoreLabel.text--;
                 }
                 else {
-                    players[ball.player].scoreLabel.text++;
+                    paddles[ball.player].scoreLabel.text++;
                 }
             }
             else if (ball.body.x < 1) {
                 scored = true;
                 if (ball.player == -1 || ball.player == 1) {
-                    players[1].scoreLabel.text--;
+                    paddles[1].scoreLabel.text--;
                 }
                 else {
-                    players[ball.player].scoreLabel.text++;
+                    paddles[ball.player].scoreLabel.text++;
                 }
             }
             else if (ball.body.x > game.world.width - ball.body.width - 1) {
                 scored = true;
                 if (ball.player == -1 || ball.player == 3) {
-                    players[3].scoreLabel.text--;
+                    paddles[3].scoreLabel.text--;
                 }
                 else {
-                    players[ball.player].scoreLabel.text++;
+                    paddles[ball.player].scoreLabel.text++;
                 }
             }
 
             if (scored) { //reset ball position
                 ball.body.position.setTo(game.world.centerX, game.world.centerY);
+                ball.tint = 0xffffff;
+                ball.body.velocity.x = game.rnd.integerInRange(-200, 200);
+                ball.body.velocity.y = game.rnd.integerInRange(-200, 200);
             }
 
-            for (var i in players) {
-                if (players[i].scoreLabel.text > 9) {
-                    this.endGame(players[i]);
+            for (var i in paddles) {
+                if (paddles[i].scoreLabel.text > 9) {
+                    this.endGame(paddles[i]);
                 }
             }
         },
         inputManagement: function () {
             var moveFactor = 2;
             if (this.cursors.left.isDown || this.cursors.up.isDown) {
-                switch (player) {
+                switch (currentPlayer) {
                     case 0:
                     case 2:
-                        players[player].position.x -= moveFactor;
+                        paddles[currentPlayer].position.x -= moveFactor;
                         break;
                     case 1:
                     case 3:
-                        players[player].position.y -= moveFactor;
+                        paddles[currentPlayer].position.y -= moveFactor;
                         break;
                 }
             }
             else if (this.cursors.right.isDown || this.cursors.down.isDown) {
-                switch (player) {
+                switch (currentPlayer) {
                     case 0:
                     case 2:
-                        players[player].position.x += moveFactor;
+                        paddles[currentPlayer].position.x += moveFactor;
                     break;
                     case 1:
                     case 3:
-                        players[player].position.y += moveFactor;
+                        paddles[currentPlayer].position.y += moveFactor;
                     break;
                 }
             }
             else {
-                switch (player) {
+                switch (currentPlayer) {
                     case 0:
                     case 2:
-                        if (game.input.activePointer.x > players[player].position.x) {
-                            players[player].position.x += moveFactor;
+                        if (game.input.activePointer.x > paddles[currentPlayer].position.x) {
+                            paddles[currentPlayer].position.x += moveFactor;
                         }
-                        else if (game.input.activePointer.x < players[player].position.x) {
-                            players[player].position.x -= moveFactor;
+                        else if (game.input.activePointer.x < paddles[currentPlayer].position.x) {
+                            paddles[currentPlayer].position.x -= moveFactor;
                         }
                     break;
                     case 1:
                     case 3:
-                        if (game.input.activePointer.y > players[player].position.y) {
-                            players[player].position.y += moveFactor;
+                        if (game.input.activePointer.y > paddles[currentPlayer].position.y) {
+                            paddles[currentPlayer].position.y += moveFactor;
                         }
-                        else if (game.input.activePointer.y < players[player].position.y) {
-                            players[player].position.y -= moveFactor;
+                        else if (game.input.activePointer.y < paddles[currentPlayer].position.y) {
+                            paddles[currentPlayer].position.y -= moveFactor;
                         }
                     break;
                 }
@@ -379,10 +367,10 @@ function Pong() {
             text.anchor.setTo(0.5, 0.5);
         },
         updateServer: function () {
-            for (var i in players) {
-                players[i].body.x;
-                players[i].body.y;
-                players[i].scoreLabel.text;
+            for (var i in paddles) {
+                paddles[i].body.x;
+                paddles[i].body.y;
+                paddles[i].scoreLabel.text;
             }
             ball.body.x;
             ball.body.y;
@@ -390,8 +378,8 @@ function Pong() {
         render: function () {
             if (debug) {
                 game.debug.body(ball);
-                for (var i in players) {
-                    game.debug.body(players[i]);
+                for (var i in paddles) {
+                    game.debug.body(paddles[i]);
                 }
             }
         }
